0c303329587cce1261a061d2cd4a3b0c80c20d97
VintusS <dragomir.mindrescu@isa.utm.md>
2025-04-27 12:32:24 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dragomir Mindrescu on 27.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
